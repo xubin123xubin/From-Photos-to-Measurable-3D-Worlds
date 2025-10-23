2868182cf27eed71c0bfa12e714de25f64614a6a
xubin123xubin <64417097+xubin123xubin@users.noreply.github.com>
2025-10-23 17:48:23 +0800

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// index.ts
+// ==============================================
+// Copyright (c) 2025 reall3d.com, MIT license
+// ==============================================
+import './reall3d/style/style.less';
+import 'virtual:svg-icons-register';
+
+import { Reall3dViewer } from './reall3d/viewer/Reall3dViewer';
+import { Reall3dViewerOptions } from './reall3d/viewer/Reall3dViewerOptions';
+import { Reall3dMapViewer } from './reall3d/mapviewer/Reall3dMapViewer';
---
+++ src/reall3d/pkg.ts
@@ -0,0 +1,6 @@
+// ==============================================
+// Copyright (c) 2025 reall3d.com, MIT license
+// ==============================================
+export { Reall3dViewer } from './viewer/Reall3dViewer';
+export { Reall3dMapViewer } from './mapviewer/Reall3dMapViewer';
+export { SplatMesh } from './meshs/splatmesh/SplatMesh';
